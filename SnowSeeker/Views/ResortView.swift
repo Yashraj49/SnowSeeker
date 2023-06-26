c9870cac3f7457ad3cf00fe49c0d11295c0ca521
Yashraj Jadhav <yashrajjadhav49@gmail.com>
2023-06-26 19:22:42 +0530

    updated app icon

--- SnowSeeker/Views/ResortView.swift
+++ SnowSeeker/Views/ResortView.swift
@@ -20,23 +20,37 @@ struct ResortView: View {
 
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading, spacing: 0) {
+            VStack(alignment: .leading, spacing: 20) {
                 Image(decorative: resort.id)
                     .resizable()
                     .scaledToFit()
-
-                if !resort.imageCredit.isEmpty {
-                    Text("Image Credit: \(resort.imageCredit)")
-                        .font(.caption)
-                        .foregroundColor(.secondary)
-                        .padding(.top, 8)
-                        .padding(.horizontal)
-                }
+                    .frame(maxWidth: 400)
+                    .frame(maxHeight: 410)
+                    .cornerRadius(50)
+                    .shadow(radius: 10)
+                    
+                    .overlay(
+                        VStack{
+                            Spacer()
+                            Text("Image Credit: \(resort.imageCredit)")
+                                .font(.caption)
+                                .foregroundColor(.white)
+                                .padding(.horizontal, 8)
+                                .padding(.bottom, 8)
+                                .background(Color.black.opacity(0.7))
+                                .cornerRadius(8)
+                        }
+                        .padding(.bottom, 2)
+                        , alignment: .bottom)
 
                 HStack {
                     if sizeClass == .compact && typeSize > .large {
-                        VStack(spacing: 10) { ResortDetailsView(resort: resort) }
-                        VStack(spacing: 10) { SkiDetailsView(resort: resort) }
+                        VStack(spacing: 10) {
+                            ResortDetailsView(resort: resort)
+                        }
+                        VStack(spacing: 10) {
+                            SkiDetailsView(resort: resort)
+                        }
                     } else {
                         ResortDetailsView(resort: resort)
                         SkiDetailsView(resort: resort)
@@ -44,26 +58,33 @@ struct ResortView: View {
                 }
                 .padding(.vertical)
                 .background(Color.primary.opacity(0.1))
-                .dynamicTypeSize(...DynamicTypeSize.xxxLarge)
+                .cornerRadius(10)
 
                 Group {
                     Text(resort.description)
-                        .padding(.vertical)
+                       
+                       // .padding(.vertical)
 
                     Text("Facilities")
                         .font(.headline)
+                        .padding(.bottom, 4)
 
                     HStack {
                         ForEach(resort.facilityTypes) { facility in
-                            Button {
+                            Button(action: {
                                 selectedFacility = facility
                                 showingFacility = true
-                            } label: {
+                            }) {
                                 facility.icon
                                     .font(.title)
+                                    .padding(8)
+                                    //.background(Color.accentColor)
+                                    .foregroundColor(.white)
+                                    .clipShape(Circle())
                             }
                         }
                     }
+                    .padding(.vertical, 4)
 
                     Button(favorites.contains(resort) ? "Remove from Favorites" : "Add to Favorites") {
                         if favorites.contains(resort) {
@@ -74,19 +95,25 @@ struct ResortView: View {
                     }
                     .buttonStyle(.borderedProminent)
                     .padding()
+
+                    Spacer()
                 }
                 .padding(.horizontal)
             }
-
+            .padding()
         }
         .navigationTitle("\(resort.name), \(resort.country)")
         .navigationBarTitleDisplayMode(.inline)
-        .alert(selectedFacility?.name ?? "More information", isPresented: $showingFacility, presenting: selectedFacility) { _ in
-        } message: { facility in
-            Text(facility.description)
+        .alert(item: $selectedFacility) { facility in
+            Alert(
+                title: Text(facility.name),
+                message: Text(facility.description),
+                dismissButton: .default(Text("Close"))
+            )
         }
     }
 }
+
 
 struct ResortView_Previews: PreviewProvider {
     static var previews: some View {
